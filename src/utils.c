a4f36a1276a1c518be94e8c5d7c4c7477a49d1ab
Nishal Kulkarni <nishalkulkarni@gmail.com>
2021-07-30 17:29:37 +0530

    utils: Add cgroup helper functions
    sd-login API doesn't provide function to get the complete system
    path for a cgroup and doesn't have a function to get unit name
    from path. These functions fill those requirements.
    
    `get_unit_cgroup_path_from_pid ()`: Reads content of /proc/$PID/cgroup
    and converts it to a complete valid path.
    
    `get_unit_name_from_path ()`: Finds the unit name by traversing
    path string and finding the first name after user@X.service which
    doesn't end in .slice

--- src/utils.c
+++ src/utils.c
@@ -2,6 +2,7 @@
 
 #include "utils.h"
 #include <glib/gstdio.h>
+#include <systemd/sd-login.h>
 
 int
 uid_cmp (gconstpointer a, gconstpointer b)
@@ -32,4 +33,82 @@ get_available_ram ()
 
   /* The value is given in kB, return bytes. */
   return g_ascii_strtoull (mem_total + 9, NULL, 10) * 1024;
+}
+
+/**
+ * get_unit_cgroup_path_from_pid:
+ * @pid: Application PID
+ *
+ * This function returns the systemd (user) unit cgroup path for the
+ * given PID. Note that the application might manage a subhierarchy of
+ * cgroups if Delegate= is set. As of now, such a sub-hierarchy is
+ * ignored and will not be returned.
+ *
+ * Returns: The full path to cgroupfs for the systemd unit, or %NULL
+ */
+gchar *
+get_unit_cgroup_path_from_pid (pid_t pid)
+{
+  g_autoptr (GError) error = NULL;
+  g_autofree gchar *contents = NULL;
+  g_autofree gchar *complete_path = NULL;
+  g_autofree gchar *unit_name = NULL;
+  g_autofree gchar *cgroup_path = NULL;
+
+  if (sd_pid_get_cgroup (pid, &contents) < 0)
+    {
+      g_warning ("Could not get cgroup path for pid: %d", pid);
+      return NULL;
+    }
+  g_strstrip (contents);
+
+  complete_path = g_strdup_printf ("%s%s", "/sys/fs/cgroup", contents);
+
+  if (sd_pid_get_user_unit (pid, &unit_name) < 0)
+    {
+      g_warning ("Could not get user unit name for pid: %d", pid);
+      return NULL;
+    }
+  g_strstrip (unit_name);
+
+  cgroup_path
+      = g_strndup (complete_path, strstr (complete_path, unit_name)
+                                      - complete_path + strlen (unit_name));
+
+  return g_steal_pointer (&cgroup_path);
+}
+
+gchar *
+get_unit_name_from_path (const gchar *path)
+{
+  g_autofree gchar *complete_path = NULL;
+  g_autofree gchar *app_unit_name = NULL;
+  gboolean after_user_service = FALSE;
+  char *token;
+
+  complete_path = g_strdup (path);
+
+  token = strtok (complete_path, "/");
+  while (token)
+    {
+      if (g_str_has_prefix (token, "user@") &&
+          g_str_has_suffix (token, ".service"))
+        {
+          after_user_service = TRUE;
+          token = strtok (NULL, "/");
+          continue;
+        }
+
+      if (after_user_service && !g_str_has_suffix (token, ".slice"))
+        {
+          if (token[0] == '_')
+            app_unit_name = g_strdup (token + 1);
+          else
+            app_unit_name = g_strdup (token);
+          break;
+        }
+      token = strtok (NULL, "/");
+    }
+
+  return g_steal_pointer (&app_unit_name);
 }
--- src/utils.h
+++ src/utils.h
@@ -5,3 +5,5 @@
 
 int uid_cmp (gconstpointer a, gconstpointer b);
 guint64 get_available_ram ();
+gchar *get_unit_cgroup_path_from_pid (pid_t pid);
+gchar *get_unit_name_from_path (const gchar *path);
