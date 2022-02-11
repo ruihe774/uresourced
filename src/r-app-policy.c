3b4c306a6c5e7d9acd68c87de0cd8bcbb693eb37
Nishal Kulkarni <nishalkulkarni@gmail.com>
2022-02-12 03:10:39 +0530

    r-app-policy: Add option to configure weights
    Allows user to set custom values for Default, Active and Boost Weights
    for CPU and IO by changing the uresourced.conf file. Incase the values
    aren't set we do not make any changes for when a window is active or boosted.
    I.e. Default[CPU/IO]Weight:100, Active[CPU/IO]Weight:100, Boost[CPU/IO]WeightInc:0

--- src/r-app-policy.c
+++ src/r-app-policy.c
@@ -4,6 +4,7 @@
 #include <glib-object.h>
 #include <glib.h>
 
+#include "uresourced-config.h"
 #include "r-app-monitor.h"
 #include "r-app-policy.h"
 
@@ -17,6 +18,15 @@ struct _RAppPolicy
 
   GDBusProxy  *proxy;
   RAppMonitor *app_monitor;
+
+  gint         default_cpu_weight;
+  gint         default_io_weight;
+
+  gint         active_cpu_weight;
+  gint         active_io_weight;
+
+  gint         boost_cpu_weight_inc;
+  gint         boost_io_weight_inc;
 };
 
 G_DEFINE_TYPE (RAppPolicy, r_app_policy, G_TYPE_OBJECT);
@@ -62,10 +72,12 @@ set_application_resources (RAppPolicy *self, RAppInfo *app)
   g_variant_builder_open (&builder, G_VARIANT_TYPE ("a(sv)"));
   g_variant_builder_add (&builder, "(sv)", "CPUWeight",
                          g_variant_new_uint64 (app->cpu_weight));
+  g_variant_builder_add (&builder, "(sv)", "IOWeight",
+                         g_variant_new_uint64 (app->io_weight));
   g_variant_builder_close (&builder);
 
-  g_info ("Setting resources on %s (CPUWeight: %ld)", app->name,
-             app->cpu_weight);
+  g_info ("Setting resources on %s (CPUWeight: %ld, IOWeight: %ld)", app->name,
+          app->cpu_weight, app->io_weight);
 
   g_dbus_proxy_call (self->proxy, "SetUnitProperties",
                      g_variant_builder_end (&builder), G_DBUS_CALL_FLAGS_NONE,
@@ -75,30 +87,113 @@ set_application_resources (RAppPolicy *self, RAppInfo *app)
 static void
 app_info_changed (gpointer *data, gpointer arg, G_GNUC_UNUSED GObject *object)
 {
-  RAppPolicy *self = R_APP_POLICY (data);
+  RAppPolicy *policy = R_APP_POLICY (data);
   RAppInfo *app = (RAppInfo *) arg;
 
   g_debug ("App Info changed: %s", app->name);
-  g_debug ("Timestamp: %ld, Boosted: %d", app->timestamp, (int)app->boosted);
+  g_debug ("Timestamp: %ld, Boosted: %d", app->timestamp, (int) app->boosted);
 
   /*
-   * For now, we only change the CPUWeight.
    * Timestamp is used for determining if an application's window is focused.
    * `boosted` is used by other sources like audio or games, to give an additional
    * boost irrespective of the application being focused.
    */
-  app->cpu_weight = (app->timestamp == -1) ? ACTIVE_WEIGHT : DEFAULT_WEIGHT;
+  app->cpu_weight = (app->timestamp == -1) ? policy->active_cpu_weight : policy->default_cpu_weight;
+  app->io_weight = (app->timestamp == -1) ? policy->active_io_weight : policy->default_io_weight;
   if (app->boosted != 0)
-    app->cpu_weight += BOOST_WEIGHT;
+    {
+      app->cpu_weight += policy->boost_cpu_weight_inc;
+      app->io_weight += policy->boost_io_weight_inc;
+    }
 
-  if (self->proxy)
-    set_application_resources (self, app);
+  if (policy->proxy)
+    set_application_resources (policy, app);
+}
+
+static inline void
+set_integer_from_key_file (GKeyFile *file,
+                           const char *group,
+                           const char *key,
+                           gint       *out)
+{
+  g_autoptr(GError) error = NULL;
+  gint value;
+
+  value = g_key_file_get_integer (file, group, key, &error);
+
+  if (error)
+    {
+      g_debug ("Could not parse key %s in group %s, keeping value %d: %s",
+               key, group, *out, error->message);
+      return;
+    }
+
+  *out = value;
+}
+
+static void
+read_config (RAppPolicy *self)
+{
+  g_autoptr(GKeyFile) file = NULL;
+  g_autoptr(GError) error = NULL;
+  g_autofree char *user_config_path = NULL;
+
+  self->default_cpu_weight = 100;
+  self->default_io_weight = 100;
+  self->active_cpu_weight = 100;
+  self->active_io_weight = 100;
+  self->boost_cpu_weight_inc = 0;
+  self->boost_io_weight_inc = 0;
+
+  file = g_key_file_new ();
+  user_config_path = g_strdup_printf ("%s/uresourced.conf", g_get_user_config_dir ());
+
+  if (!g_key_file_load_from_file (file, user_config_path, G_KEY_FILE_NONE, NULL))
+    {
+      g_debug ("Could not read %s, trying default location", user_config_path);
+
+      if (!g_key_file_load_from_file (file, SYSCONFDIR "/uresourced.conf", G_KEY_FILE_NONE, &error))
+        {
+          g_warning ("Could not read default configuration file: %s", error->message);
+          goto out;
+        }
+    }
+
+  set_integer_from_key_file (file, "AppBoost", "DefaultCPUWeight", &self->default_cpu_weight);
+  self->default_cpu_weight = CLAMP (self->default_cpu_weight, 1, 10000);
+
+  set_integer_from_key_file (file, "AppBoost", "DefaultIOWeight", &self->default_io_weight);
+  self->default_io_weight = CLAMP (self->default_io_weight, 1, 10000);
+
+  set_integer_from_key_file (file, "AppBoost", "ActiveCPUWeight", &self->active_cpu_weight);
+  self->active_cpu_weight = CLAMP (self->active_cpu_weight, 1, 10000);
+
+  set_integer_from_key_file (file, "AppBoost", "ActiveIOWeight", &self->active_io_weight);
+  self->active_io_weight = CLAMP (self->active_io_weight, 1, 10000);
+
+  set_integer_from_key_file (file, "AppBoost", "BoostCPUWeightInc", &self->boost_cpu_weight_inc);
+  self->boost_cpu_weight_inc = CLAMP (self->boost_cpu_weight_inc, 0, 10000 - self->active_cpu_weight);
+
+  set_integer_from_key_file (file, "AppBoost", "BoostIOWeightInc", &self->boost_io_weight_inc);
+  self->boost_io_weight_inc = CLAMP (self->boost_io_weight_inc, 0, 10000 - self->active_io_weight);
+
+out:
+  g_info ("CPU Configuration: Default CPUWeight: %d, Active CPUWeight: %d, Boost CPUWeight: %d",
+          self->default_cpu_weight,
+          self->active_cpu_weight,
+          self->boost_cpu_weight_inc);
+  g_info ("IO Configuration: Default IOWeight: %d, Active IOWeight: %d, Boost IOWeight: %d",
+          self->default_io_weight,
+          self->active_io_weight,
+          self->boost_io_weight_inc);
 }
 
 void
 r_app_policy_start (RAppPolicy *self, RAppMonitor *monitor)
 {
   self->app_monitor = monitor;
+
+  read_config (self);
 
   g_signal_connect_object (monitor, "changed", G_CALLBACK (app_info_changed),
                            self, G_CONNECT_SWAPPED);
